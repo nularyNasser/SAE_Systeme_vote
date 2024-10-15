9b721da06e391bc90618e2299e7263e3e96d1944
nularyNasser <nahamed.sveil@gmail.com>
2024-10-16 00:11:24 +0200

    Compteur des vote de chaque electeur

--- vote.py
+++ vote.py
@@ -5,6 +5,7 @@ nombre_electeur = int(input("Nombre d'electeur: "))
 choix_candidat: int
 choix_deja_fait = []
 
+
 for nb_scrutin in range(nombre_electeur):
     print("-----------------------------------")
     print(f"SCRUTIN NUMERO {nb_scrutin+1}") # Nombre de scrutin
@@ -14,19 +15,22 @@ for nb_scrutin in range(nombre_electeur):
         print("---------")
         for candidat in range(len(candidats)): # Affichage des candidat
             print(candidats[candidat])
-
-        choix_candidat = int(input(f"Candidat numero {i+1} : "))
-        while (choix_candidat <= 0 or choix_candidat > len(candidats)) and (choix_candidat in choix_deja_fait):
-            choix_candidat = int(input(f"Candidat numero {i+1} : "))
-
-        choix_deja_fait.append(choix_candidat)
-        choix_candidat -= 1
-        print(f"Vous avez choisi le candidat numéro {candidats[choix_candidat]}")
-        print(choix_deja_fait)
-
+        
 classement = {
     1: ['A', 'B', 'C', 'D'],
     2: ['A', 'C', 'B', 'D'],
     3: ['B', 'A', 'C', 'D']
 }
 
+compteurA = 0
+compteurB = 0
+
+for i in range(len(classement)):
+    print(classement[i+1])
+    if classement[i+1][0] == 'A':
+        compteurA += 1
+    elif classement[i+1][0] == 'B':
+        compteurB += 1
+
+print('A:', compteurA)
+print('B:', compteurB)
